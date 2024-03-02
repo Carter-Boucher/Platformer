760f5a0f266ae111aaf52f50d253a00ffd702840
Carter Boucher <carterboucher85@gmail.com>
2024-03-02 00:22:30 -0400

    videos done

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,78 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <iostream>
+#include <vector>
 
-int main(int argc, char* args[]) {
-	std::cout << "hello" << std::endl;
-	std::cin.get();
+#include "RenderWindow.hpp"
+#include "Entity.hpp"
+#include "Math.hpp"
+#include "utils.hpp"
+
+int main(int argc, char* args[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+
+	RenderWindow window("GAME v1.0", 1280, 720);
+
+	SDL_Texture* grassTexture = window.loadTexture("res/gfx/ground_grass_1.png");
+
+	std::vector<Entity> entities = {Entity(Vector2f(0, 148), grassTexture),
+						 	   Entity(Vector2f(30, 148), grassTexture),
+						 	   Entity(Vector2f(60, 148), grassTexture),
+						 	   Entity(Vector2f(90, 148), grassTexture)};
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	const float timeStep = 0.01f;
+	float accumulator = 0.0f;
+	float currentTime = utils::hireTimeInSeconds();
+
+	while (gameRunning)
+	{
+		int startTicks = SDL_GetTicks();
+		float newTime = utils::hireTimeInSeconds();
+		float frameTime = newTime - currentTime;
+		currentTime = newTime;
+
+		accumulator += frameTime;
+
+		while(accumulator >= timeStep){
+			// Get our controls and events
+			while (SDL_PollEvent(&event))
+			{
+				if (event.type == SDL_QUIT)
+					gameRunning = false;
+			}
+			accumulator -= timeStep;
+		}
+
+		const float alpha = accumulator / timeStep;
+
+		window.clear();
+
+		for(Entity& e: entities){
+			window.render(e);
+		}
+
+		window.display();
+
+		int frameTicks = SDL_GetTicks() - startTicks;
+
+		if(frameTicks < 1000/window.getRefreshRate()){
+			SDL_Delay(1000/window.getRefreshRate());
+		}
+	}
+
+
+
+	window.cleanUp();
+	SDL_Quit();
+
 	return 0;
 }
