e2e93ddccf79e07eb6b5947649b32f41c3398169
Carter Boucher <carterboucher85@gmail.com>
2024-03-03 02:04:31 -0400

    fixed -Wreorder errors

--- src/main.cpp
+++ src/main.cpp
@@ -16,21 +16,21 @@ int main(int argc, char* args[])
 	if (!(IMG_Init(IMG_INIT_PNG)))
 		std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
 
-	RenderWindow window("GAME v1.0", 1280, 720);
+	RenderWindow window("GAME v1.0", 1280,);
 
 	SDL_Texture* grassTexture = window.loadTexture("res/gfx/ground_grass_1.png");
 
 	std::vector<Entity> entities = {Entity(Vector2f(0, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(30, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(60, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(90, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(120, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(150, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(180, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(210, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(240, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(270, 156), grassTexture, Vector2f(32,32)),
-									Entity(Vector2f(300, 156), grassTexture, Vector2f(32,32))};
+					Entity(Vector2f(30, 156), grassTexture, Vector2f(32,32)),
+					Entity(Vector2f(60, 156), grassTexture, Vector2f(32,32)),
+					Entity(Vector2f(90, 156), grassTexture, Vector2f(32,32)),
+					Entity(Vector2f(120, 156), grassTexture, Vector2f(32,32)),
+					Entity(Vector2f(150, 156), grassTexture, Vector2f(32,32)),
+					Entity(Vector2f(180, 156), grassTexture, Vector2f(32,32)),
+					Entity(Vector2f(210, 156), grassTexture, Vector2f(32,32)),
+					Entity(Vector2f(240, 156), grassTexture, Vector2f(32,32)),
+					Entity(Vector2f(270, 156), grassTexture, Vector2f(32,32)),
+					Entity(Vector2f(300, 156), grassTexture, Vector2f(32,32))};
 
 	SDL_Texture* skyTexture = window.loadTexture("res/gfx/sky.png");
 	Entity sky(Vector2f(0, 0), skyTexture, Vector2f(735,414));
