565f469c100048597d04ea1659792f2a5e039d8e
Carter Boucher <carterboucher85@gmail.com>
2024-04-12 17:20:11 -0300

    heal and damage fix

--- src/character.cpp
+++ src/character.cpp
@@ -9,6 +9,7 @@ character::character(float health, std::vector<int> damage){
 }
 
 bool character::heal(float p_health){
+	if(p_health < 1) return false;
 	if(health > 0) {
 		health += p_health;
 		return true;
@@ -19,8 +20,10 @@ bool character::heal(float p_health){
 }
 
 bool character::takeDamage(float p_health){
+	if(p_health < 1) return false;
 	if(health - p_health < 0) {
 		dead = true;
+		health = 0;
 		return false;
 	}
 	health -= p_health;
